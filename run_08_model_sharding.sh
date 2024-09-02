4856b9749614d859a7c481276e0d04be577c9f75
Abhishek Goswami <abgoswam@gmail.com>
2024-09-02 19:30:46 +0000

    got regular 4k train on llama2 to work with multiple tp sizes. next try mistral

--- run_08_model_sharding.sh
+++
@@ -1,8 +0,0 @@
-python tools/checkpoint_util.py \
-	--target_tensor_parallel_size 2 \
-	--target_pipeline_parallel_size 1 \
-	--load_dir /path/to/megatron/weights/ \
-	--save_dir /path/to/sharded/weights/ \
-	--model_type llama2 \
-	--true_vocab_size 32000 \
-	--bf16
