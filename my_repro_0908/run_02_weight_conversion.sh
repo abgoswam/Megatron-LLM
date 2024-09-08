1a775a16bc709ab782725d5c21452d14f2c2765b
Abhishek Goswami <abgoswam@gmail.com>
2024-09-08 19:52:21 +0000

    trying repro for mistral. stage 03 correctness_verification

--- my_repro_0908/run_02_weight_conversion.sh
+++ my_repro_0908/run_02_weight_conversion.sh
@@ -1,15 +1,20 @@
-# llama2
-python hf_to_megatron.py \
-    --size 7 \
-    --out temp_out_llama2_7b \
-    --model-path meta-llama/Llama-2-7b-hf \
-    --cache-dir temp_cache_llama2_7b \
-    llama2
+# python weights_conversion/hf_to_megatron.py \
+#     llama2 \
+#     --size=7 \
+# 	--out=/path/to/megatron/weights/ \
+#     --cache-dir=/path/to/llama-2-7b/
+
+# # llama2
+# python hf_to_megatron.py \
+#     --size 7 \
+#     --out temp_out_llama2_7b \
+#     --model-path meta-llama/Llama-2-7b-hf \
+#     --cache-dir temp_cache_llama2_7b \
+#     llama2
 
 # mistral
-python hf_to_megatron.py \
+python ./weights_conversion/hf_to_megatron.py \
     --size 7 \
-    --out out_mistral_7b \
+    --out ./my_repro_0908/my_repro_ckpts/repro_out_mistral_7b \
     --model-path mistralai/Mistral-7B-v0.1 \
-    --cache-dir cache_mistral_7b \
     mistral
