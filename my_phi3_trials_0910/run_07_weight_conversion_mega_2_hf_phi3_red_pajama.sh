97ba2f5aa42bda73bc94e3b3b045036c6dff9a13
Abhishek Goswami <abgoswam@gmail.com>
2024-09-16 17:20:13 +0000

    add reshard/ mega2hf conversion scripts for red_pajama trained data

---
+++ my_phi3_trials_0910/run_07_weight_conversion_mega_2_hf_phi3_red_pajama.sh
@@ -0,0 +1,19 @@
+# TODO (agoswami): at this point we may need to pass in a tokenizer.model file
+# logs show this:
+# LlamaTokenizerFast loaded from huggingface
+# vocab_file not set, assuming same tokenizer.model used by llama LlamaTokenizerFast
+
+python weights_conversion/megatron_to_hf.py \
+    --input_dir=/mnt/synthdatastore/agoswami/models_04_postlaborday/out_phi31_red_pajama_ckpt1500_reshard/ \
+	--output_dir=/mnt/synthdatastore/agoswami/models_04_postlaborday/out_phi31_red_pajama_ckpt1500_hf/
+
+# python weights_conversion/megatron_to_hf.py \
+#     --input_dir=./my_phi3_trials_0910/ckpts/out_phi3_reshard/ \
+# 	--output_dir=./my_phi3_trials_0910/ckpts/out_phi3_hf/ \
+#     --vocab_file=./my_phi3_trials_0910/ckpts/Phi-3-mini-4k-instruct/tokenizer.model \
+#     --override_special_tokens "bos=<s>" "eos=<|endoftext|>" "pad=<|endoftext|>" "unk=<unk>"
+
+# python weights_conversion/megatron_to_hf.py \
+#     --input_dir=./my_phi3_trials_0910/ckpts/out_phi3_reshard/ \
+# 	--output_dir=./my_phi3_trials_0910/ckpts/out_phi3_hf/ \
+#     --vocab_file=./my_phi3_trials_0910/ckpts/Phi-3-mini-4k-instruct/tokenizer.model
