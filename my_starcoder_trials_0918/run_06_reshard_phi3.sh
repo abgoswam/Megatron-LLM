c86a26294ef280f0a843bc065632af29f520e322
Abhishek Goswami <abgoswam@gmail.com>
2024-09-22 00:59:27 +0000

    done with redpajama conversion

--- my_starcoder_trials_0918/run_06_reshard_phi3.sh
+++ my_starcoder_trials_0918/run_06_reshard_phi3.sh
@@ -5,8 +5,8 @@ TP=8
 python tools/checkpoint_util.py \
 	--target_tensor_parallel_size 1 \
 	--target_pipeline_parallel_size 1 \
-	--load_dir /mnt/synthdatastore/agoswami/models_04_postlaborday/my_starcoder_trials_0918/ckpts/out_phi3_orig2_tp8_save_LR0/ \
-	--save_dir /mnt/synthdatastore/agoswami/models_04_postlaborday/my_starcoder_trials_0918/ckpts/out_phi3_orig2_reshard500_save_LR0/ \
+	--load_dir /mnt/synthdatastore/agoswami/models_04_postlaborday/my_starcoder_trials_0920/ckpts/out_phi3_orig1_tp8_redpajama \
+	--save_dir /mnt/synthdatastore/agoswami/models_04_postlaborday/my_starcoder_trials_0920/ckpts/out_phi3_orig1_reshard10000_redpajama \
 	--model_type mistral \
 	--true_vocab_size 32064 \
 	--bf16
--- my_starcoder_trials_0918/run_07_weight_conversion_mega_2_hf_phi3.sh
+++ my_starcoder_trials_0918/run_07_weight_conversion_mega_2_hf_phi3.sh
@@ -4,8 +4,8 @@
 # vocab_file not set, assuming same tokenizer.model used by llama LlamaTokenizerFast
 
 python weights_conversion/megatron_to_hf.py \
-    --input_dir=/mnt/synthdatastore/agoswami/models_04_postlaborday/my_starcoder_trials_0918/ckpts/out_phi3_orig2_reshard500_save_LR0/ \
-	--output_dir=/mnt/synthdatastore/agoswami/models_04_postlaborday/my_starcoder_trials_0918/ckpts/out_phi3_orig2_reshard500_save_LR0_hf/ \
+    --input_dir  /mnt/synthdatastore/agoswami/models_04_postlaborday/my_starcoder_trials_0920/ckpts/out_phi3_orig1_reshard10000_redpajama/ \
+	--output_dir /mnt/synthdatastore/agoswami/models_04_postlaborday/my_starcoder_trials_0920/ckpts/out_phi3_orig1_reshard10000_redpajama_hf/ \
     --model phi3 \
     --vocab_extra_ids_list "<|endoftext|>" \
     --override_special_tokens "pad=<|endoftext|>" # need to re-insert because it got overwritten L686
